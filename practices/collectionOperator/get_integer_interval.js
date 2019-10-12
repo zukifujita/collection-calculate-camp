9977f7bc71ec6e2431f0c1ceebc1db7bdc805eb9
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-10-13 02:22:23 +0900

    Added collectionOperator

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //write code here
+  var minValue = Math.min(number_a, number_b);
+  var maxValue = Math.max(number_a, number_b);
+  var result = [];
+
+  for(minValue; minValue <= maxValue; minValue++){
+    result.push(minValue);
+  }
+
+  if (number_a > number_b) {
+    result.reverse();
+  }
+
+  return result;
 }
 
 module.exports = get_integer_interval;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  return number_a, number_b.filter(element => number_a % 2 ? number_a++ : number_b++);
+  var minValue = Math.min(number_a, number_b);
+  var maxValue = Math.max(number_a, number_b);
+  var result = [];
+
+  for(minValue; minValue <= maxValue; minValue++){
+    if (minValue % 2 == 0){
+      result.push(minValue);
+    }
+  }
+
+  if (number_a > number_b) {
+    result.reverse();
+  }
+
+  return result;
 }
 
 module.exports = get_integer_interval_2;
